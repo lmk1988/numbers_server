21679f0ac56949347d6630078b1ef07386d27503
Ming Kit <luk_mk@hotmail.com>
2017-04-19 01:41:37 +0800

    Added generateRandomString function

--- app_modules/components/hash.js
+++ app_modules/components/hash.js
@@ -1,5 +1,8 @@
-const config = require("config");
-const bcrypt = require("bcrypt");
+"use strict";
+const config    = require("config");
+const bcrypt    = require("bcrypt");
+const crypto    = require("crypto");
+const Promise   = require("bluebird");
 
 /**
  * @param {String} password plain text password
@@ -18,5 +21,18 @@ function comparePassword(plainPassword, hashedPassword){
     return bcrypt.compare(plainPassword, hashedPassword);
 }
 
-exports.hashPlainPassword = hashPlainPassword;
-exports.comparePassword = comparePassword;
+/**
+ * Generates a random string
+ * @param {*} length length of the desired random string
+ */
+function generateRandomString(length){
+    //Length divide by 2 because a byte generated 2 characters
+    return Promise.promisify(crypto.randomBytes)(Math.ceil(length/2))
+    .then(function(randomBytes){
+        //Convert bytes to hex
+        return randomBytes.toString("hex");
+    });
+}
+
+exports.hashPlainPassword   = hashPlainPassword;
+exports.comparePassword     = comparePassword;
