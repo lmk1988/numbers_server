9b2b2bd2dad8b0f1d9eb3dd78186cc1ab396dda0
Ming Kit <luk_mk@hotmail.com>
2017-04-19 01:38:42 +0800

    Updated constants for new database struct

--- app_modules/models/constants.js
+++ app_modules/models/constants.js
@@ -1,25 +1,29 @@
-
+"use strict";
 
 const TABLES = {
-    USER_INFO : "user_info",
-    USER_PASSWORD : "user_password",
-    USER_PHONE_BOOTH : "user_phone_booth",
-    USER_PHONE_BOOTH_EXTRA : "user_phone_booth_extra"
+    USER_INFO               : "user_info",
+    USER_PASSWORD           : "user_password",
+    USER_ACCESS             : "user_access",
+    USER_PHONE_BOOTH        : "user_phone_booth",
+    USER_PHONE_BOOTH_EXTRA  : "user_phone_booth_extra"
 };
 
 const FIELDS = {
-    USER_ID : "user_id",
-    NAME : "name",
-    EMAIL : "email",
-    PASSWORD_HASH : "password_hash",
-    PHONE_BOOTH_ID : "phone_booth_id",
-    IMG_URL : "img_url",
-    CONTACT_NUM : "contact_num",
-    CONTACT_EXT : "contact_ext",
-    PHONE_BOOTH_EXTRA_ID : "phone_booth_extra_id",
-    DETAILS : "details",
-    CREATED_AT : "created_at",
-    UPDATED_AT : "updated_at"
+    USER_ID                 : "user_id",
+    NAME                    : "name",
+    EMAIL                   : "email",
+    PASSWORD_HASH           : "password_hash",
+    ACCESS_ID               : "access_id",
+    ACCESS_TOKEN            : "access_token",
+    REFRESH_TOKEN           : "refresh_token",
+    PHONE_BOOTH_ID          : "phone_booth_id",
+    IMG_URL                 : "img_url",
+    CONTACT_NUM             : "contact_num",
+    CONTACT_EXT             : "contact_ext",
+    PHONE_BOOTH_EXTRA_ID    : "phone_booth_extra_id",
+    DETAILS                 : "details",
+    CREATED_AT              : "created_at",
+    UPDATED_AT              : "updated_at"
 };
 
 const CONSTANTS = {
