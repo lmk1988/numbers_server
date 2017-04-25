d23eb08182b469447673b874e8c322fcb323b666
Ming Kit <luk_mk@hotmail.com>
2017-04-26 03:56:00 +0800

    Fix easy auth

--- app_modules/middlewares/auth.js
+++ app_modules/middlewares/auth.js
@@ -5,9 +5,19 @@ const oauth     = rootRequire("app_modules/components/oauth2");
 
 function middlewareAttachSessionAccessToken(req, res, next){
     //This allows consistent API to handle both client and mobile app API
-    if(req.body && !req.body.access_token && req.session && req.session.access_token){
-        //Attach session access_token to body for oauth to verify
-        req.body.access_token = req.session.access_token;
+    if(req.session && req.session.access_token){
+        const access_token = req.session.access_token;
+        if(req.method == "POST" || req.method == "PUT" || req.method == "DELETE"){
+            if(req.body && !req.body.access_token){
+                //Attach session access_token to body for oauth to verify
+                req.body.access_token = access_token;
+            }
+        }else if(req.method == "GET"){
+            if(req.query && !req.query.access_token){
+                //Attach session access_token to query for oauth to verify
+                req.query.access_token = access_token;
+            }
+        }
     }
 
     next();
