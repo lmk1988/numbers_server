6884a5ffde749bf12fe5a3bdb3b1b331c70e8142
Ming Kit <luk_mk@hotmail.com>
2017-04-26 05:28:52 +0800

    Added change password API

--- app_modules/api/users/index.js
+++ app_modules/api/users/index.js
@@ -5,9 +5,16 @@ const router     = express.Router();
 const register          = require("./register");
 const resetPassword     = require("./reset_password");
 const profile           = require("./profile");
+const changePassword    = require("./change_password");
+
+const auth_middleware = rootRequire("app_modules/middlewares/auth");
+
 router.use("/profile", profile);
 
 router.post("/register", register);
 router.post("/reset_password", resetPassword);
+
+//Routes that require authorization
+router.post("/change_password", auth_middleware, changePassword);
 
 module.exports = router;
