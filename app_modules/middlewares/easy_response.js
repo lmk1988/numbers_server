46b5ce62cb370282458828049b266f8d8c7c9d76
Ming Kit <luk_mk@hotmail.com>
2017-04-27 19:06:59 +0800

    Clean up easy_response

--- app_modules/middlewares/easy_response.js
+++ app_modules/middlewares/easy_response.js
@@ -7,27 +7,28 @@ function easy_response(req, res, next){
 
     res.easy_success = function(data){
         res.status(200).json({ status : "success" , data : data || {} });
-    }
+    };
 
     res.easy_invalid = function(msg){
         res.status(400).json({ status : "fail", message : (msg || "Invalid Fields") });
-    }
+    };
 
     res.easy_not_found = function(msg){
         res.status(404).json({ status : "fail", message : (msg || "Not Found") });
-    }
+    };
 
     res.easy_forbidden = function(msg){
         res.status(403).json({ status : "fail", message : (msg || "Forbiddened") });
-    }
+    };
 
     res.easy_conflict = function(msg){
         res.status(409).json({ status : "fail", message : (msg || "Conflict") });
-    }
+    };
 
     res.easy_error = function(msg){
         res.status(500)({ status : "error", message : (msg || "Server Error") });
-    }
+    };
+
     next();
 }
 
